97ad67d537adb3914926544c33c99eae8f2a34fa
Suhong Jin <suhongjin94@gmail.com>
2015-11-14 09:10:29 -0600

    Revert "Removing old project"
    This reverts commit 4e69aede06a14623eacc91f30f2b12186ddcae03.

---
+++ android/MyApplication/app/src/main/java/com/jin/chan/emotipic/MainScreen.java
@@ -0,0 +1,36 @@
+package com.jin.chan.emotipic;
+
+import com.jin.chan.emotipic.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+
+/**
+ * An example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see SystemUiHider
+ */
+public class MainScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, Camera2BasicFragment.newInstance())
+                    .commit();
+        }
+    }
+}
