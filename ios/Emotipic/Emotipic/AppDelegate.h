2d5894ec622d83ab0b9d80b17669dd1ad0d6663b
Suhong Jin <suhongjin94@gmail.com>
2015-11-17 15:24:11 -0600

    add default ios app

---
+++ ios/Emotipic/Emotipic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Emotipic
+//
+//  Created by Suhong Jin on 11/17/15.
+//  Copyright (c) 2015 Emotipic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/Emotipic/Emotipic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Emotipic
+//
+//  Created by Suhong Jin on 11/17/15.
+//  Copyright (c) 2015 Emotipic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
